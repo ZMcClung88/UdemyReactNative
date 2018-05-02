c568811525c8ab2d1165b8dd13ab64c1508c6777
Zac McClung <zac.mcclung370@gmail.com>
2018-05-02 13:55:51 -0600

    saving changes

--- index.ios.js
+++ index.ios.js
@@ -4,14 +4,15 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
-import AlbumList from './src/components/AlbumList';
+// import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
   <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
-    <AlbumList />
+    {/* <AlbumList /> */}
   </View>
+  // <Text>Some Text</Text>
 );
 
 // Render it to the device
