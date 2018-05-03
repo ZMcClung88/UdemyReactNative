ea444c62534a9cc8416d5d960e9f6263f83e5832
Zac McClung <zac.mcclung370@gmail.com>
2018-05-02 22:23:38 -0600

    list component boilerplate

--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,13 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
-// import AlbumList from './src/components/AlbumList';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
   <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
-    {/* <AlbumList /> */}
+    <AlbumList />
   </View>
   // <Text>Some Text</Text>
 );
